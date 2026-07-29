6e2c382fa8a872702976fa1d08e1e3171ad42098
quantran113 <quantnb.adsagency@gmail.com>
2026-07-29 22:17:02 +0700

    Fix hidden Cua Hang VIP in NPC Santa dialog menu by adding Cua Hang VIP option directly to Santa menu list

--- SRC/src/nro/models/npc_list/Santa.java
+++ SRC/src/nro/models/npc_list/Santa.java
@@ -28,6 +28,7 @@ public class Santa extends Npc {
             }
             List<String> menu = new ArrayList<>(Arrays.asList(
                     "Cửa hàng",
+                    "Cửa hàng\nVIP",
                     "Mở rộng\nHành trang\nRương đồ",
                     "Nhập mã\nquà tặng",
                     "Cửa hàng\nHạn sử dụng",
@@ -50,59 +51,31 @@ public class Santa extends Npc {
     public void confirmMenu(Player player, int select) {
         if (canOpenNpc(player)) {
             Item pGG = InventoryService.gI().findItem(player.inventory.itemsBag, 459);
-            int soLuong = 0;
-            if (pGG != null) {
-                soLuong = pGG.quantity;
-            }
+            boolean hasCoupon = (pGG != null && pGG.quantity >= 1);
 
             if (this.mapId == 5 || this.mapId == 13 || this.mapId == 20) {
                 if (player.idMark.isBaseMenu()) {
-                    switch (select) {
-                        case 0:
-                            ShopService.gI().opendShop(player, "SANTA", false);
-                            break;
-                        case 1:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_GIAM_GIA_1", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
-                            }
-                            break;
-                        case 2:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
-                            } else {
-                                Input.gI().createFormGiftCode(player);
-                            }
-                            break;
-                        case 3:
-                            if (soLuong >= 1) {
-                                Input.gI().createFormGiftCode(player);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
-                            }
-                            break;
-                        case 4:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_HEAD", false);
-                            }
-                            break;
-                        case 5:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_HEAD", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_DANH_HIEU", false);
-                            }
-                            break;
-                        case 6:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_DANH_HIEU", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SHOP_VIP", false);
-                            }
-                            break;
+                    if (hasCoupon) {
+                        switch (select) {
+                            case 0 -> ShopService.gI().opendShop(player, "SANTA", false);
+                            case 1 -> ShopService.gI().opendShop(player, "SANTA_GIAM_GIA_1", false);
+                            case 2 -> ShopService.gI().opendShop(player, "SHOP_VIP", false);
+                            case 3 -> ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
+                            case 4 -> Input.gI().createFormGiftCode(player);
+                            case 5 -> ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
+                            case 6 -> ShopService.gI().opendShop(player, "SANTA_HEAD", false);
+                            case 7 -> ShopService.gI().opendShop(player, "SANTA_DANH_HIEU", false);
+                        }
+                    } else {
+                        switch (select) {
+                            case 0 -> ShopService.gI().opendShop(player, "SANTA", false);
+                            case 1 -> ShopService.gI().opendShop(player, "SHOP_VIP", false);
+                            case 2 -> ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
+                            case 3 -> Input.gI().createFormGiftCode(player);
+                            case 4 -> ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
+                            case 5 -> ShopService.gI().opendShop(player, "SANTA_HEAD", false);
+                            case 6 -> ShopService.gI().opendShop(player, "SANTA_DANH_HIEU", false);
+                        }
                     }
                 }
             }
