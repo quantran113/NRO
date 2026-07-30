5b29cdf270716beb9666d69ea33ada91574a6a97
quantran113 <quantnb.adsagency@gmail.com>
2026-07-30 13:52:05 +0700

    Update code 2026-07-30 13:52:05

--- SRC/run.sh
+++ SRC/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-java -server -Dfile.encoding=UTF-8 -cp "build/classes:20.jar:lib/*" nro.models.server.ServerManager
+JAVA_BIN=$(which java 2>/dev/null || find /usr/lib/jvm /usr/bin /usr/local -name java -type f 2>/dev/null | head -n 1)
+if [ -z "$JAVA_BIN" ]; then
+    JAVA_BIN="java"
+fi
+$JAVA_BIN -server -Dfile.encoding=UTF-8 -cp "build/classes:20.jar:lib/*" nro.models.server.ServerManager
