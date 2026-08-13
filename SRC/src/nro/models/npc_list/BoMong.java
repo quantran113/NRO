eddf8f65d4429682edcafd83e49b50b4906eec10
quantran113 <quantnb.adsagency@gmail.com>
2026-08-13 22:49:27 +0700

    Update code 2026-08-13 22:49:27

--- SRC/src/nro/models/npc_list/BoMong.java
+++ SRC/src/nro/models/npc_list/BoMong.java
@@ -30,10 +30,8 @@ public class BoMong extends Npc {
     public void openBaseMenu(Player player) {
         if (!TaskService.gI().checkDoneTaskTalkNpc(player, this)) {
             if (canOpenNpc(player)) {
-                if (this.mapId == 47 || this.mapId == 84) {
-                    this.createOtherMenu(player, ConstNpc.BASE_MENU,
-                            "Ngươi muốn có thêm ngọc thì chịu khó làm vài nhiệm vụ sẽ được ngọc thưởng", "Nhiệm vụ\nhàng ngày", "Nhiệm vụ\nthành tích", "Nạp Ngọc", "Điểm danh", "Từ chối");
-                }
+                this.createOtherMenu(player, ConstNpc.BASE_MENU,
+                        "Ngươi muốn có thêm ngọc thì chịu khó làm vài nhiệm vụ sẽ được ngọc thưởng", "Nhiệm vụ\nhàng ngày", "Nhiệm vụ\nthành tích", "Nạp Ngọc", "Điểm danh", "Từ chối");
             }
         }
     }
@@ -41,66 +39,64 @@ public class BoMong extends Npc {
     @Override
     public void confirmMenu(Player player, int select) {
         if (canOpenNpc(player)) {
-            if (this.mapId == 47 || this.mapId == 84) {
-                if (player.idMark.isBaseMenu()) {
-                    switch (select) {
-                        case 0 -> {
-                            if (player.playerTask.sideTask.template != null) {
-                                String npcSay = "Nhiệm vụ hiện tại: " + player.playerTask.sideTask.getName() + " ("
-                                        + player.playerTask.sideTask.getLevel() + ")"
-                                        + "\nHiện tại đã hoàn thành: " + player.playerTask.sideTask.count + "/"
-                                        + player.playerTask.sideTask.maxCount + " ("
-                                        + player.playerTask.sideTask.getPercentProcess() + "%)\nSố nhiệm vụ còn lại trong ngày: "
-                                        + player.playerTask.sideTask.leftTask + "/" + ConstTask.MAX_SIDE_TASK;
-                                this.createOtherMenu(player, ConstNpc.MENU_OPTION_PAY_SIDE_TASK,
-                                        npcSay, "Trả nhiệm\nvụ", "Hủy nhiệm\nvụ");
-                            } else {
-                                this.createOtherMenu(player, ConstNpc.MENU_OPTION_LEVEL_SIDE_TASK,
-                                        "Tôi có vài nhiệm vụ theo cấp bậc, "
-                                        + "sức cậu có thể làm được cái nào?",
-                                        "Dễ", "Bình thường", "Khó", "Từ chối");
+            if (player.idMark.isBaseMenu()) {
+                switch (select) {
+                    case 0 -> {
+                        if (player.playerTask.sideTask.template != null) {
+                            String npcSay = "Nhiệm vụ hiện tại: " + player.playerTask.sideTask.getName() + " ("
+                                    + player.playerTask.sideTask.getLevel() + ")"
+                                    + "\nHiện tại đã hoàn thành: " + player.playerTask.sideTask.count + "/"
+                                    + player.playerTask.sideTask.maxCount + " ("
+                                    + player.playerTask.sideTask.getPercentProcess() + "%)\nSố nhiệm vụ còn lại trong ngày: "
+                                    + player.playerTask.sideTask.leftTask + "/" + ConstTask.MAX_SIDE_TASK;
+                            this.createOtherMenu(player, ConstNpc.MENU_OPTION_PAY_SIDE_TASK,
+                                    npcSay, "Trả nhiệm\nvụ", "Hủy nhiệm\nvụ");
+                        } else {
+                            this.createOtherMenu(player, ConstNpc.MENU_OPTION_LEVEL_SIDE_TASK,
+                                    "Tôi có vài nhiệm vụ theo cấp bậc, "
+                                    + "sức cậu có thể làm được cái nào?",
+                                    "Dễ", "Bình thường", "Khó", "Từ chối");
+                        }
+                    }
+                    case 1 -> {
+                        AchievementService.gI().openAchievementUI(player);
+                    }
+                    case 2 -> {
+                        Input.gI().createFormTradeGem(player);
+                    }
+                    case 3 -> {
+                        if (player.lastCheckIn != null) {
+                            LocalDate last = player.lastCheckIn.toLocalDate();
+                            LocalDate today = LocalDate.now();
+                            if (last.isEqual(today)) {
+                                Service.gI().sendThongBao(player, "Bạn đã điểm danh hôm nay rồi!");
+                                return;
                             }
                         }
-                        case 1 -> {
-                            AchievementService.gI().openAchievementUI(player);
-                        }
-                        case 2 -> {
-                            Input.gI().createFormTradeGem(player);
-                        }
-                        case 3 -> {
-                            if (player.lastCheckIn != null) {
-                                LocalDate last = player.lastCheckIn.toLocalDate();
-                                LocalDate today = LocalDate.now();
-                                if (last.isEqual(today)) {
-                                    Service.gI().sendThongBao(player, "Bạn đã điểm danh hôm nay rồi!");
-                                    return;
-                                }
-                            }
-                            player.lastCheckIn = LocalDateTime.now();
-                            player.inventory.gem += 10000;
-                            Item item457 = ItemService.gI().createNewItem((short) 457);
-                            item457.quantity = 100;
-                            item457.itemOptions.add(new Item.ItemOption(30, 0));
-                            InventoryService.gI().addItemBag(player, item457);
-                            PlayerService.gI().sendInfoHpMpMoney(player);
-                            InventoryService.gI().sendItemBags(player);
+                        player.lastCheckIn = LocalDateTime.now();
+                        player.inventory.gem += 10000;
+                        Item item457 = ItemService.gI().createNewItem((short) 457);
+                        item457.quantity = 100;
+                        item457.itemOptions.add(new Item.ItemOption(30, 0));
+                        InventoryService.gI().addItemBag(player, item457);
+                        PlayerService.gI().sendInfoHpMpMoney(player);
+                        InventoryService.gI().sendItemBags(player);
 
-                            Service.gI().sendThongBao(player, "Điểm danh thành công! Bạn nhận được 10.000 ngọc và 100 thỏi vàng.");
-                        }
+                        Service.gI().sendThongBao(player, "Điểm danh thành công! Bạn nhận được 10.000 ngọc và 100 thỏi vàng.");
+                    }
 
-                    }
-                } else if (player.idMark.getIndexMenu() == ConstNpc.MENU_OPTION_LEVEL_SIDE_TASK) {
-                    switch (select) {
-                        case 0, 1, 2 ->
-                            TaskService.gI().changeSideTask(player, (byte) select);
-                    }
-                } else if (player.idMark.getIndexMenu() == ConstNpc.MENU_OPTION_PAY_SIDE_TASK) {
-                    switch (select) {
-                        case 0 ->
-                            TaskService.gI().paySideTask(player);
-                        case 1 ->
-                            TaskService.gI().removeSideTask(player);
-                    }
+                }
+            } else if (player.idMark.getIndexMenu() == ConstNpc.MENU_OPTION_LEVEL_SIDE_TASK) {
+                switch (select) {
+                    case 0, 1, 2 ->
+                        TaskService.gI().changeSideTask(player, (byte) select);
+                }
+            } else if (player.idMark.getIndexMenu() == ConstNpc.MENU_OPTION_PAY_SIDE_TASK) {
+                switch (select) {
+                    case 0 ->
+                        TaskService.gI().paySideTask(player);
+                    case 1 ->
+                        TaskService.gI().removeSideTask(player);
                 }
             }
         }
