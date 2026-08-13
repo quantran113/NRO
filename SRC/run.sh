f1c62011d1d0972a2cfed69b0589235a1ca8d803
quantran113 <quantnb.adsagency@gmail.com>
2026-08-13 21:16:36 +0700

    Update code 2026-08-13 21:16:36

--- SRC/run.sh
+++ SRC/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-java -server -Dfile.encoding=UTF-8 -cp "build/classes:20.jar:lib/*" nro.models.server.ServerManager
+while true; do
+    echo "🚀 Đang khởi chạy NRO Game Server..."
+    java -server -Xms256m -Xmx1024m -Dfile.encoding=UTF-8 -cp "build/classes:20.jar:lib/*" nro.models.server.ServerManager
+    echo "⚠️ Game Server bị tắt! Tự động khởi động lại sau 2 giây..."
+    sleep 2
+done
